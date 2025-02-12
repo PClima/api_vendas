56a7c10a54e936f94f2752b272ad6ac714279104
Pedro Cordeiro Lima <cordeirolima.pedro@gmail.com>
2025-02-12 16:10:34 -0300

    typeorm repository implementation

--- src/products/infrastructure/typeorm/repositories/products-typeorm.repository.int-spec.ts
+++ src/products/infrastructure/typeorm/repositories/products-typeorm.repository.int-spec.ts
@@ -4,6 +4,8 @@ import { NotFoundError } from '@/common/domain/errors/not-found-error'
 import { randomUUID } from 'crypto'
 import { ProductsDataBuilder } from '../../testing/helpers/products-data-builder'
 import { Product } from '../entities/products.entitiy'
+import { ConflictError } from '@/common/domain/errors/conflict-error'
+import { ProductModel } from '@/products/domain/models/products.model'
 
 describe('ProductsTypeormRepository integration Tests', () => {
   let ormRepository: ProductsTypeormRepository
@@ -67,6 +69,241 @@ describe('ProductsTypeormRepository integration Tests', () => {
       })
       const result = await ormRepository.insert(data)
       expect(result.name).toEqual(data.name)
+    })
+  })
+
+  describe('update', () => {
+    it('Should throw error when id not found in update', async () => {
+      const data = ProductsDataBuilder({})
+
+      await expect(ormRepository.update(data)).rejects.toThrow(
+        new NotFoundError(`Product not found unsing id ${data.id}`),
+      )
+    })
+
+    it('Should update a product by id in update', async () => {
+      const data = ProductsDataBuilder({})
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+      product.name = 'Updated name'
+
+      const result = await ormRepository.update(product)
+      expect(result.name).toEqual('Updated name')
+    })
+  })
+
+  describe('delete', () => {
+    it('Should throw error when id not found in delete', async () => {
+      const id = randomUUID()
+
+      await expect(ormRepository.delete(id)).rejects.toThrow(
+        new NotFoundError(`Product not found unsing id ${id}`),
+      )
+    })
+
+    it('Should delete a product by id in delete', async () => {
+      const data = ProductsDataBuilder({})
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+
+      await ormRepository.delete(data.id)
+
+      const result = await ormRepository.productsRepository.findOneBy({
+        id: data.id,
+      })
+      expect(result).toBeNull()
+    })
+  })
+
+  describe('findByName', () => {
+    it('Should throw error when name not found in findByName', async () => {
+      const name = 'fake-name'
+
+      await expect(ormRepository.findByName(name)).rejects.toThrow(
+        new NotFoundError(`Product not found unsing name fake-name`),
+      )
+    })
+
+    it('Should find a product by name in findByName', async () => {
+      const data = ProductsDataBuilder({ name: 'test-name' })
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+
+      const result = await ormRepository.findByName(product.name)
+      expect(result.name).toEqual('test-name')
+    })
+  })
+
+  describe('conflictingName', () => {
+    it('Should throw error when found a product by name in conflictingName', async () => {
+      const data = ProductsDataBuilder({ name: 'test-name' })
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+
+      await expect(ormRepository.conflictingName('test-name')).rejects.toThrow(
+        new ConflictError(`Name 'test-name' already used on another product`),
+      )
+    })
+
+    it('Should not find a product by name in conflictingName', async () => {
+      expect.assertions(0)
+      await ormRepository.conflictingName('fake-name')
+    })
+  })
+
+  describe('findAllByIds', () => {
+    it('Should return a empty array when not found any products in findAllByIds', async () => {
+      const productsIds = [
+        { id: '88984790-f719-496b-bee6-ccda23b64f05' },
+        { id: randomUUID() },
+      ]
+
+      const result = await ormRepository.findAllByIds(productsIds)
+      expect(result).toHaveLength(0)
+      expect(result).toEqual([])
+    })
+
+    it('Should return a array with products when found products in findAllByIds', async () => {
+      const data = [
+        ProductsDataBuilder({ name: 'Product 1' }),
+        ProductsDataBuilder({ name: 'Product 2' }),
+      ]
+      const product = testDataSource.manager.create(Product, data)
+      await testDataSource.manager.save(product)
+
+      const productsIds = product.map(item => ({ id: item.id }))
+      //First test should return only one product
+      let result = await ormRepository.findAllByIds([productsIds[0]])
+      expect(result).toHaveLength(1)
+      //Second test should return all products
+      result = await ormRepository.findAllByIds(productsIds)
+      expect(result).toHaveLength(2)
+    })
+  })
+
+  describe('search', () => {
+    it('Should apply only pagination when other params is null', async () => {
+      const arrange = Array(16).fill(ProductsDataBuilder({}))
+      arrange.map(element => delete element.id)
+      const data = testDataSource.manager.create(Product, arrange)
+      await testDataSource.manager.save(data)
+
+      const result = await ormRepository.search({
+        page: 1,
+        per_page: 10,
+        sort: null,
+        sort_dir: null,
+        filter: null,
+      })
+
+      expect(result.total).toEqual(16)
+      expect(result.items).toHaveLength(10)
+      expect(result.sort).toEqual('created_at')
+    })
+
+    it('Should order by created_at DESC when search params are null', async () => {
+      const created_at = new Date()
+      const models: ProductModel[] = []
+      const arrange = Array(16).fill(ProductsDataBuilder({}))
+      arrange.forEach((element, index) => {
+        delete element.id
+        models.push({
+          ...element,
+          name: `Product ${index}`,
+          created_at: new Date(created_at.getTime() + index),
+        })
+      })
+      const data = testDataSource.manager.create(Product, models)
+      await testDataSource.manager.save(data)
+
+      const result = await ormRepository.search({
+        page: 1,
+        per_page: 10,
+        sort: null,
+        sort_dir: null,
+        filter: null,
+      })
+
+      expect(result.items[0].name).toEqual('Product 15')
+      expect(result.items[9].name).toEqual('Product 6')
+    })
+
+    it('Should apply paginate and sort', async () => {
+      const created_at = new Date()
+      const models: ProductModel[] = []
+      'badec'.split('').forEach((element, index) => {
+        models.push({
+          ...ProductsDataBuilder({}),
+          name: element,
+          created_at: new Date(created_at.getTime() + index),
+        })
+      })
+      const data = testDataSource.manager.create(Product, models)
+      await testDataSource.manager.save(data)
+
+      let result = await ormRepository.search({
+        page: 1,
+        per_page: 2,
+        sort: 'name',
+        sort_dir: 'asc',
+        filter: null,
+      })
+
+      expect(result.items[0].name).toEqual('a')
+      expect(result.items[1].name).toEqual('b')
+      expect(result.items.length).toEqual(2)
+
+      result = await ormRepository.search({
+        page: 1,
+        per_page: 2,
+        sort: 'name',
+        sort_dir: 'desc',
+        filter: null,
+      })
+
+      expect(result.items[0].name).toEqual('e')
+      expect(result.items[1].name).toEqual('d')
+      expect(result.items.length).toEqual(2)
+    })
+
+    it('should search using filter, sort and paginate', async () => {
+      const created_at = new Date()
+      const models: ProductModel[] = []
+      const values = ['test', 'a', 'TEST', 'b', 'TeSt']
+      values.forEach((element, index) => {
+        models.push({
+          ...ProductsDataBuilder({}),
+          name: element,
+          created_at: new Date(created_at.getTime() + index),
+        })
+      })
+      const data = testDataSource.manager.create(Product, models)
+      await testDataSource.manager.save(data)
+
+      let result = await ormRepository.search({
+        page: 1,
+        per_page: 2,
+        sort: 'name',
+        sort_dir: 'asc',
+        filter: 'TEST',
+      })
+
+      expect(result.items[0].name).toEqual('test')
+      expect(result.items[1].name).toEqual('TeSt')
+      expect(result.items.length).toEqual(2)
+      expect(result.total).toEqual(3)
+
+      result = await ormRepository.search({
+        page: 2,
+        per_page: 2,
+        sort: 'name',
+        sort_dir: 'asc',
+        filter: 'TEST',
+      })
+
+      expect(result.items[0].name).toEqual('TEST')
+      expect(result.items.length).toEqual(1)
+      expect(result.total).toEqual(3)
     })
   })
 })
--- src/products/infrastructure/typeorm/repositories/products-typeorm.repository.ts
+++ src/products/infrastructure/typeorm/repositories/products-typeorm.repository.ts
@@ -8,10 +8,12 @@ import {
   ProductId,
   ProductsRepository,
 } from '@/products/domain/repositories/products.repository'
-import { Repository } from 'typeorm'
+import { ILike, In, Repository } from 'typeorm'
 import { Product } from '../entities/products.entitiy'
 import { dataSource } from '@/common/infrastructure/typeorm'
 import { NotFoundError } from '@/common/domain/errors/not-found-error'
+import { ConflictError } from '@/common/domain/errors/conflict-error'
+import { totalmem } from 'os'
 
 export class ProductsTypeormRepository implements ProductsRepository {
   //Setting the fields that can be sorted
@@ -24,16 +26,28 @@ export class ProductsTypeormRepository implements ProductsRepository {
     this.productsRepository = dataSource.getRepository(Product)
   }
 
-  findByName(name: string): Promise<ProductModel> {
-    throw new Error('Method not implemented.')
+  async findByName(name: string): Promise<ProductModel> {
+    const product = await this.productsRepository.findOneBy({ name })
+    if (!product) {
+      throw new NotFoundError(`Product not found unsing name ${name}`)
+    }
+    return product
   }
 
-  findAllByIds(productIds: ProductId[]): Promise<ProductModel[]> {
-    throw new Error('Method not implemented.')
+  async findAllByIds(productIds: ProductId[]): Promise<ProductModel[]> {
+    const ids = productIds.map(productId => productId.id)
+    const productsFound = await this.productsRepository.find({
+      where: { id: In(ids) },
+    })
+
+    return productsFound
   }
 
-  conflictingName(name: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async conflictingName(name: string): Promise<void> {
+    const products = await this.productsRepository.findOneBy({ name })
+    if (products) {
+      throw new ConflictError(`Name '${name}' already used on another product`)
+    }
   }
 
   //Creating a new instance of the ProductModel
@@ -50,16 +64,44 @@ export class ProductsTypeormRepository implements ProductsRepository {
     return this._get(id)
   }
 
-  update(model: ProductModel): Promise<ProductModel> {
-    throw new Error('Method not implemented.')
+  async update(model: ProductModel): Promise<ProductModel> {
+    await this._get(model.id)
+    await this.productsRepository.update({ id: model.id }, model)
+    return model
   }
 
-  delete(id: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async delete(id: string): Promise<void> {
+    await this._get(id)
+    await this.productsRepository.delete({ id })
   }
 
-  search(props: SearchInput): Promise<SearchOutput<ProductModel>> {
-    throw new Error('Method not implemented.')
+  async search(props: SearchInput): Promise<SearchOutput<ProductModel>> {
+    //Validating the sort and sort_dir fields are correct
+    const validSort = this.sortableFields.includes(props.sort) || false
+    const dirOps = ['asc', 'desc']
+    const validSortDir =
+      (props.sort_dir && dirOps.includes(props.sort_dir.toLowerCase())) || false
+
+    //Setting the orderByField and orderByDir based on the validation, if no set default values
+    const orderByField = validSort ? props.sort : 'created_at'
+    const orderByDir = validSortDir ? props.sort_dir : 'desc'
+
+    const [products, total] = await this.productsRepository.findAndCount({
+      ...(props.filter && { where: { name: ILike(props.filter) } }),
+      order: { [orderByField]: orderByDir },
+      take: props.per_page,
+      skip: (props.page - 1) * props.per_page,
+    })
+
+    return {
+      items: products,
+      per_page: props.per_page,
+      total,
+      current_page: props.page,
+      sort: orderByField,
+      sort_dir: orderByDir,
+      filter: props.filter,
+    }
   }
 
   //Creating a _get to reuse the search method
